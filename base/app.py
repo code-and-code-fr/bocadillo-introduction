f2be7920d17b54cac81afc41bc3abdaf0d70798c
mikael <mbriolet.ma@gmail.com>
2019-09-08 15:47:10 +0200

    Create the first page

--- base/app.py
+++ base/app.py
@@ -1,7 +1,17 @@
 """Application definition."""
-from bocadillo import App, discover_providers
+from bocadillo import App, configure, Templates
+
+import socketio
+
 
 app = App()
-discover_providers("base.providerconf")
+configure(app)
+templates = Templates()
 
-# Create routes here.
+sio = socketio.AsyncServer(async_mode="asgi")
+app.mount("/sio", socketio.ASGIApp(sio))
+
+
+@app.route("/")
+async def index(req, res):
+    res.html = await templates.render("index.html")
