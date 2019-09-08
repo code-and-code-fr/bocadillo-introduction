17792f50a7caa18cb39075d4d066ae77ca0ac51d
mikael <mbriolet.ma@gmail.com>
2019-09-08 17:42:51 +0200

    application done

--- base/app.py
+++ base/app.py
@@ -1,17 +1,23 @@
 """Application definition."""
-from bocadillo import App, configure, Templates
+from bocadillo import App, Templates, static
 
 import socketio
 
 
 app = App()
-configure(app)
 templates = Templates()
 
 sio = socketio.AsyncServer(async_mode="asgi")
 app.mount("/sio", socketio.ASGIApp(sio))
+app.mount("/socket.io", static("node_modules/socket.io-client/dist"))
 
 
 @app.route("/")
-async def index(req, res):
-    res.html = await templates.render("index.html")
+async def index(request, response):
+    response.html = await templates.render("index.html")
+
+
+@sio.on("message")
+async def broadcast(sid, data: str):
+    print("message:", data)
+    await sio.emit("response", data)
